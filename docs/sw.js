4b75947e9a430d5987f7c38f50739d0861eacd14
xinping0014-create <xinping0014@gmail.com>
2025-12-10 16:35:34 +0800

    Add PWA files (manifest, service worker, update index.html)

---
+++ docs/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'jamesbirthday-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './styles.css',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((resp) => resp || fetch(evt.request))
+  );
+});
